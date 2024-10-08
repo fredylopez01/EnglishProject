0eb83857f3e6c46011d8fb3415be04c3720757dd
Fredy Lopez <133995369+fredylopez01@users.noreply.github.com>
2024-10-07 23:29:56 -0500

    Hago pequeños ajuste

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { HistoryComponent } from './pages/history/history.component';
+import { ImpactComponent } from './pages/impact/impact.component';
+import { OpportunitiesComponent } from './pages/opportunities/opportunities.component';
+import { EnglishRelationComponent } from './pages/english-relation/english-relation.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path: "history",
+    component: HistoryComponent
+  },
+  {
+    path: "impact",
+    component: ImpactComponent
+  },
+  {
+    path: "opportunities",
+    component: OpportunitiesComponent
+  },
+  {
+    path: "englishRelation",
+    component: EnglishRelationComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HistoryComponent } from './pages/history/history.component';
+import { EventCardComponent } from './components/event-card/event-card.component';
+import { ImpactComponent } from './pages/impact/impact.component';
+import { OpportunitiesComponent } from './pages/opportunities/opportunities.component';
+import { EnglishRelationComponent } from './pages/english-relation/english-relation.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    HistoryComponent,
+    EventCardComponent,
+    ImpactComponent,
+    OpportunitiesComponent,
+    EnglishRelationComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
