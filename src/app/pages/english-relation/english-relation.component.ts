13b13ffd16cc882e0814e5710faccf96f907e59e
Fredy Lopez <133995369+fredylopez01@users.noreply.github.com>
2024-10-07 23:28:30 -0500

    Agrego página de relacion ingles y tecnología

---
+++ src/app/pages/english-relation/english-relation.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-english-relation',
+  templateUrl: './english-relation.component.html',
+  styleUrls: ['./english-relation.component.scss']
+})
+export class EnglishRelationComponent {
+
+}
